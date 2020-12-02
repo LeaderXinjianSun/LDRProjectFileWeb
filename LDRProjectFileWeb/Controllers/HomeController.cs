2cfa2db293475c6bb5e78c34dcc1e2e9304f7467
鑫建 孙 <xinjian_sun@sina.com>
2020-12-02 16:03:12 +0800

    显示项目图片

--- LDRProjectFileWeb/Controllers/HomeController.cs
+++ LDRProjectFileWeb/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using LDRProjectFileWeb.Models;
 using LDRProjectFileWeb.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,10 +13,12 @@ namespace LDRProjectFileWeb.Controllers
     public class HomeController : Controller
     {
         private readonly IProjectRepository _projectRepository;
+        private readonly IWebHostEnvironment _hostingEnvironment;
 
-        public HomeController(IProjectRepository projectRepository)
+        public HomeController(IProjectRepository projectRepository, IWebHostEnvironment hostingEnvironment)
         {
             _projectRepository = projectRepository;
+            _hostingEnvironment = hostingEnvironment;
         }
         public IActionResult Index()
         {
@@ -85,6 +89,33 @@ namespace LDRProjectFileWeb.Controllers
         {
             _projectRepository.Delete(id);
             return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ViewResult ProjectImage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ProjectImage(ProjectImageCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.File != null && model.File.FileName.Contains(".jpg"))
+                {
+                    //必须将图像上传到wwwroot中的images文件夹
+                    //而要获取wwwroot文件夹的路径，我们需要注入 ASP.NET Core提供的HostingEnvironment服务
+                    //通过HostingEnvironment服务去获取wwwroot文件夹的路径
+                    string uploadsFolder = _hostingEnvironment.WebRootPath;
+                    string filePath = Path.Combine(uploadsFolder, "1.jpg");
+                    //因为使用了非托管资源，所以需要手动进行释放
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        //使用IFormFile接口提供的CopyTo()方法将文件复制到wwwroot/images文件夹
+                        model.File.CopyTo(fileStream);
+                    }
+                }
+            }
+            return View();
         }
     }
 }
---
+++ LDRProjectFileWeb/ViewModels/ProjectImageCreateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LDRProjectFileWeb.ViewModels
+{
+    public class ProjectImageCreateViewModel
+    {
+        [Display(Name = "文件")]
+        public IFormFile File { get; set; }
+    }
+}
